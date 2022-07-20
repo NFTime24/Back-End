3c53168ab5730899dc5e079b6c73b6913d5a6fdc
deukyunlee <dukedev1004@gmail.com>
2022-07-20 17:16:19 +0900

    initial commit

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write([]byte("hello"))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":3000", nil)
 }
