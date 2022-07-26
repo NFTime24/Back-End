19358c29e84ddd42b6482f71607b24969d48f70b
deukyunlee <dukedev1004@gmail.com>
2022-07-26 17:54:39 +0900

    FEAT: addition to work table

--- main.go
+++ main.go
@@ -287,23 +287,24 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 }
 
 type NFTInfo struct {
-	Id             int    `json:"id"`
-	Artist_id      int    `json:"artist_id"`
-	Price          int    `json:"price"`
-	File_id        int    `json:"file_id"`
-	Filesize       int    `json:"filesize"`
-	Thumbnail_id   int    `json:"thumbnail_id"`
-	Name           string `json:"name"`
-	Description    string `json:"description"`
-	Category       string `json:"category"`
-	Owner_address  string `json:"owner_address"`
-	Filename       string `json:"filename"`
-	Filetype       string `json:"filetype"`
-	Path           string `json:"path"`
-	Thumbnail_path string `json:"thumbnail_path"`
-	Artist_address string `json:"artist_address"`
-	Artist_name    string `json:"artist_name"`
+	NftId             int    `json:"nft_id"`
+	OwnerAddress      string `json:"owner_address"`
+	WorkName          string `json:"work_name"`
+	WorkPrice         int    `json:"work_price"`
+	Description       string `json:"description"`
+	Category          string `json:"category"`
+	Filename          string `json:"filename"`
+	Filesize          int    `json:"filesize"`
+	Filetype          string `json:"filetype"`
+	Path              string `json:"path"`
+	ThumbnailPath     string `json:"thumbnail_path"`
+	ArtistName        string `json:"artist_name"`
+	ArtistProfilePath string `json:"artist_profile_path`
+	ArtistAddress     string `json:"artist_address"`
 }
+
+// nft_id, work_price, filesize
+// owner_address, work_name, description, category, filename, filetype, path, thumbnail_path, artist_name, artist_profile_path, artist_address
 
 type NFTInfoBundle struct {
 	NFTInfos []NFTInfo `json:"nft_infos"`
@@ -312,27 +313,19 @@ type NFTInfoBundle struct {
 func getNFTInfo(w http.ResponseWriter, r *http.Request) {
 	nft_owner := r.URL.Query().Get("owner_address")
 
-	db := db.DbConn()
+	db := dbConn()
 	selDB, err := db.Query(fmt.Sprintf(`
-	select wft.*,
-	a.address as "artist_address", a.name as "artist_name"
-	from
-		(select wf.*,
-		t.path as "thumnail_path"
-		from
-			(select 
-			w.id, w.name, w.artist_id, 
-			ifnull(w.price, 0) as "price", ifnull(w.description, "") as "description", 
-			ifnull(w.category, "") as "category", ifnull(w.owner_address, "") as "owner_address", w.file_id,
-			f.filename, f.filesize, f.filetype, f.path, ifnull(f.thumbnail_id, 0) as "thumbnail_id"
-			from protocol_camp.work as w
-			left join protocol_camp.file as f
-			on w.file_id = f.id
-			where w.owner_address = "%s") as wf
-		left join protocol_camp.file as t
-		on wf.thumbnail_id = t.id) as wft
-	left join protocol_camp.artist as a
-	on wft.artist_id = a.id
+	select 
+		n.nft_id, n.owner_address, w.name as "work_name", w.price as "work_price", w.description, w.category,
+		f.filename, f.filesize, f.filetype, f.path, t.path as "thumbnail_path", 
+		a.name as "artist_name", ifnull(p.path, "") as "artist_profile_path", a.address as "artist_address"
+	from protocol_camp.nft as n
+	left join protocol_camp.work as w on n.work_id = w.id
+	left join protocol_camp.file as f on w.file_id = f.id
+	left join protocol_camp.file as t on f.thumbnail_id = t.id
+	left join protocol_camp.artist as a on w.artist_id = a.id
+	left join protocol_camp.file as p on a.profile_id = p.id
+	where n.owner_address = "%s"
 	`, nft_owner))
 	if err != nil {
 		panic(err.Error())
@@ -340,34 +333,30 @@ func getNFTInfo(w http.ResponseWriter, r *http.Request) {
 
 	nftinfos := NFTInfoBundle{}
 	for selDB.Next() {
-		var id, artist_id, price, file_id, filesize, thumbnail_id int
-		var name, description, category, owner_address, filename, filetype, path, thumbnail_path, artist_address, artist_name string
+		var nft_id, work_price, filesize int
+		var owner_address, work_name, description, category, filename, filetype, path, thumbnail_path, artist_name, artist_profile_path, artist_address string
 		//var fname, fsize, ftype, path string
-		err = selDB.Scan(
-			&id, &name, &artist_id, &price, &description, &category, &owner_address,
-			&file_id, &filename, &filesize, &filetype, &path, &thumbnail_id, &thumbnail_path,
-			&artist_address, &artist_name)
+		err = selDB.Scan(&nft_id, &owner_address, &work_name, &work_price, &description, &category, &filename, &filesize, &filetype, &path,
+			&thumbnail_path, &artist_name, &artist_profile_path, &artist_address)
 		if err != nil {
 			panic(err.Error())
 		}
 		nftinfo := &NFTInfo{}
 
-		nftinfo.Id = id
-		nftinfo.Name = name
-		nftinfo.Artist_id = artist_id
-		nftinfo.Price = price
+		nftinfo.NftId = nft_id
+		nftinfo.OwnerAddress = owner_address
+		nftinfo.WorkName = work_name
+		nftinfo.WorkPrice = work_price
 		nftinfo.Description = description
 		nftinfo.Category = category
-		nftinfo.Owner_address = owner_address
-		nftinfo.File_id = file_id
 		nftinfo.Filename = filename
 		nftinfo.Filesize = filesize
 		nftinfo.Filetype = filetype
 		nftinfo.Path = path
-		nftinfo.Thumbnail_id = thumbnail_id
-		nftinfo.Thumbnail_path = thumbnail_path
-		nftinfo.Artist_address = artist_address
-		nftinfo.Artist_name = artist_name
+		nftinfo.ThumbnailPath = thumbnail_path
+		nftinfo.ArtistName = artist_name
+		nftinfo.ArtistProfilePath = artist_profile_path
+		nftinfo.ArtistAddress = artist_address
 
 		nftinfos.NFTInfos = append(nftinfos.NFTInfos, *nftinfo)
 	}
