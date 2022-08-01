7910d458650e140108ff0d58be39b20b137db560
deukyunlee <dukedev1004@gmail.com>
2022-08-01 14:54:49 +0900

    test

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"deukyunlee/protocol-camp/db"
-	"encoding/json"
+	"database/sql"
 	"fmt"
-	"io"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,351 +11,184 @@ import (
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
-	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/joho/godotenv"
 )
 
-type Artist struct {
-	Aid      int
-	Aname    string
-	Aaddress string
-}
-
-type WorkInfo struct {
-	Workid          int    `json: work_id`
-	Workname        string `json: work_name`
-	Workprice       int    `json: work_price`
-	Workdescription string `json: work_description`
-	Workcatogory    string `json: work_category`
-	Artistname      string `json: artist_name`
-	Artistaddress   string `json: artist_address`
-}
-
-func specificWork(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	workinfo := WorkInfo{}
-	workinfos := []WorkInfo{}
-	db := db.DbConn()
-	w_name := r.URL.Query().Get("w_name")
-	rows, err := db.Query(`select w.id, w.name, w.price, w.description, w.category, a.name, a.address  from work as w join artist as a where w.artist_id = a.id and w.name = ?;`, w_name)
-	if err != nil {
-		panic(err)
+func dbConn() (db *sql.DB) {
+	er := godotenv.Load(".env")
+	if er != nil {
+		panic(er.Error())
 	}
-
-	for rows.Next() {
-		var workid, workprice int
-		var workname, workdescription, workcategory, artistname, artistaddress string
-
-		err := rows.Scan(&workid, &workname, &workprice, &workdescription, &workcategory, &artistname, &artistaddress)
-		if err != nil {
-			panic(err)
-		}
-		workinfo.Workid = workid
-		workinfo.Workname = workname
-		workinfo.Workprice = workprice
-		workinfo.Workdescription = workdescription
-		workinfo.Workcatogory = workcategory
-		workinfo.Artistname = artistname
-		workinfo.Artistaddress = artistaddress
-
-		workinfos = append(workinfos, workinfo)
-	}
-	jData, err := json.Marshal(workinfos)
-
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(jData)
-	db.Close()
-}
-
-func workInfoSearch(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	workinfo := WorkInfo{}
-	workinfos := []WorkInfo{}
-	db := db.DbConn()
-	rows, err := db.Query("select w.id, w.name, w.price, w.description, w.category, a.name, a.address from work as w join artist as a where w.artist_id = a.id")
-	if err != nil {
-		panic(err)
-	}
-
-	for rows.Next() {
-		var workid, workprice int
-		var workname, workdescription, workcategory, artistname, artistaddress string
-
-		err := rows.Scan(&workid, &workname, &workprice, &workdescription, &workcategory, &artistname, &artistaddress)
-		if err != nil {
-			panic(err)
-		}
-		workinfo.Workid = workid
-		workinfo.Workname = workname
-		workinfo.Workprice = workprice
-		workinfo.Workdescription = workdescription
-		workinfo.Workcatogory = workcategory
-		workinfo.Artistname = artistname
-		workinfo.Artistaddress = artistaddress
-
-		workinfos = append(workinfos, workinfo)
-	}
-	jData, err := json.Marshal(workinfos)
-
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(jData)
-	db.Close()
-}
-func saveHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-	workname := r.FormValue("workname")
-	artist := r.FormValue("artist")
-	price := r.FormValue("price")
-	description := r.FormValue("description")
-	uploadFile, header, err := r.FormFile("upload_file")
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
-		return
-	}
-	//fileByte = uploadFile.Open()
-
-	defer uploadFile.Close()
-
-	filename := header.Filename
-	dirname := "./assets/uploadimage"
-	os.MkdirAll(dirname, 0777)
-	textname := strings.IndexByte(header.Filename, '.')
-	extensionText := filename[textname:]
-	if extensionText == ".mp4" {
-		dirname = "./assets/uploadvideo"
-	}
-	filepath := fmt.Sprintf("%s/%s", dirname, header.Filename)
-	file, err := os.Create(filepath)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, err)
-		return
-	}
-	defer file.Close()
-	fmt.Print(file.Name())
-
-	io.Copy(file, uploadFile)
-	w.WriteHeader(http.StatusOK)
-	// fmt.Fprint(w, filepath)
-
-	info, err := os.Stat(filepath)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("filesize:", info.Size()/1024)
-	filesize := int(info.Size() / 1024)
-	//fmt.Println("type of filesize: ", reflect.TypeOf(filesize))
-	fmt.Println("workname: ", workname)
-	fmt.Println("artist: ", artist)
-	fmt.Println("price: ", price)
-	fmt.Println("description: ", description)
-
-	text := strings.IndexByte(filename, '.')
-	extension := filename[text:]
-	fmt.Println("extension: ", extension)
-
-	path := filepath[2:]
-	fmt.Println("filepath: ", filepath[2:])
-
-	fmt.Println("filename: ", filename)
-	var filetype string
-	switch extension {
-	case ".png":
-		filetype = "image/png"
-	case ".jpg":
-		filetype = "image/jpg"
-
-	case ".jpeg":
-		filetype = "image/jpeg"
-	case ".gif":
-		filetype = "image/gif"
-	case ".mp4":
-		filetype = "video/mp4"
-	}
-
-	db := db.DbConn()
-	var id int
-	var artistId int
-	err = db.QueryRow("SELECT id FROM file order by id desc limit 1").Scan(&id)
-	if err != nil {
-		id = 0
-		//log.Fatal(err)
-	}
-	//fmt.Println(id)
-	id = id + 1
-	insForm, err := db.Prepare("INSERT INTO file(id, filename, filesize, filetype, path) VALUES(?,?,?,?,?)")
-	if err != nil {
-		fmt.Println("file")
-		panic(err.Error())
-	}
-	result, err := insForm.Exec(id, filename, filesize, filetype, path)
-	if err != nil {
-		fmt.Println("file")
-		log.Fatal(err)
-	} else {
-		log.Println("data inserted successfully . . .")
-	}
-	n, err := result.RowsAffected()
-	fmt.Println(n, "rows affected for file table")
-	log.Printf("Successfully Uploaded File\n")
-
-	err = db.QueryRow("SELECT id FROM work order by id desc limit 1").Scan(&artistId)
-	if err != nil {
-		id = 0
-	}
-
-	insFormWork, err := db.Prepare("INSERT INTO work(id, name, artist_id,price, description,category,file_id) VALUES(?,?,?,?,?,?,?)")
-	if err != nil {
-		fmt.Println("work")
-		panic(err.Error())
-	} else {
-		log.Println("data insert successfully . . .")
-	}
-
-	resultWork, err := insFormWork.Exec(artistId+1, workname, artist, price, description, filetype, id)
-	if err != nil {
-		fmt.Println("work")
-		log.Fatal(err)
-	}
-	num, err := resultWork.RowsAffected()
-	fmt.Println(num, "rows affected for work table")
-	log.Printf("Successfully Uploaded File\n")
-	db.Close()
-	fmt.Fprint(w, "file uploaded to"+filepath+"\n", num, " rows affected")
-}
-
-type NFTInfo struct {
-	NftId             int    `json:"nft_id"`
-	OwnerAddress      string `json:"owner_address"`
-	WorkName          string `json:"work_name"`
-	WorkPrice         int    `json:"work_price"`
-	Description       string `json:"description"`
-	Category          string `json:"category"`
-	Filename          string `json:"filename"`
-	Filesize          int    `json:"filesize"`
-	Filetype          string `json:"filetype"`
-	Path              string `json:"path"`
-	ThumbnailPath     string `json:"thumbnail_path"`
-	ArtistName        string `json:"artist_name"`
-	ArtistProfilePath string `json:"artist_profile_path`
-	ArtistAddress     string `json:"artist_address"`
-}
-
-// nft_id, work_price, filesize
-// owner_address, work_name, description, category, filename, filetype, path, thumbnail_path, artist_name, artist_profile_path, artist_address
-
-type NFTInfoBundle struct {
-	NFTInfos []NFTInfo `json:"nft_infos"`
-}
-
-func getNFTInfo(w http.ResponseWriter, r *http.Request) {
-	nft_owner := r.URL.Query().Get("owner_address")
-
-	db := db.DbConn()
-	selDB, err := db.Query(fmt.Sprintf(`
-	select 
-		n.nft_id, n.owner_address, w.name as "work_name", w.price as "work_price", w.description, w.category,
-		f.filename, f.filesize, f.filetype, f.path, t.path as "thumbnail_path", 
-		a.name as "artist_name", ifnull(p.path, "") as "artist_profile_path", a.address as "artist_address"
-	from protocol_camp.nft as n
-	left join protocol_camp.work as w on n.work_id = w.id
-	left join protocol_camp.file as f on w.file_id = f.id
-	left join protocol_camp.file as t on f.thumbnail_id = t.id
-	left join protocol_camp.artist as a on w.artist_id = a.id
-	left join protocol_camp.file as p on a.profile_id = p.id
-	where n.owner_address = "%s"
-	`, nft_owner))
+	dbDriver := os.Getenv("DB_Driver")
+	dbUser := os.Getenv("DB_User")
+	dbPass := os.Getenv("DB_Password")
+	dbName := os.Getenv("DB_Name")
+	dbHost := os.Getenv("DB_HOST")
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+")/"+dbName)
 	if err != nil {
 		panic(err.Error())
 	}
+	return db
+}
 
-	nftinfos := NFTInfoBundle{}
+type upfile struct {
+	ID    int
+	Fname string
+	Fsize string
+	Ftype string
+	Path  string
+	Count int
+}
+
+// var tmplIndex = *template.Template
+var tmpl = template.Must(template.ParseGlob("templates/uploadfile.html"))
+
+// var tmplIndex = template.Must(template.ParseGlob("templates/index.html"))
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM file ORDER BY id DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+	upld := upfile{}
+	res := []upfile{}
 	for selDB.Next() {
-		var nft_id, work_price, filesize int
-		var owner_address, work_name, description, category, filename, filetype, path, thumbnail_path, artist_name, artist_profile_path, artist_address string
-		//var fname, fsize, ftype, path string
-		err = selDB.Scan(&nft_id, &owner_address, &work_name, &work_price, &description, &category, &filename, &filesize, &filetype, &path,
-			&thumbnail_path, &artist_name, &artist_profile_path, &artist_address)
+		var id int
+		var fname, fsize, ftype, path string
+		err = selDB.Scan(&id, &fname, &fsize, &ftype, &path)
 		if err != nil {
 			panic(err.Error())
 		}
-		nftinfo := &NFTInfo{}
+		upld.ID = id
+		upld.Fname = fname
+		upld.Fsize = fsize
+		upld.Ftype = ftype
+		upld.Path = path
+		res = append(res, upld)
 
-		nftinfo.NftId = nft_id
-		nftinfo.OwnerAddress = owner_address
-		nftinfo.WorkName = work_name
-		nftinfo.WorkPrice = work_price
-		nftinfo.Description = description
-		nftinfo.Category = category
-		nftinfo.Filename = filename
-		nftinfo.Filesize = filesize
-		nftinfo.Filetype = filetype
-		nftinfo.Path = path
-		nftinfo.ThumbnailPath = thumbnail_path
-		nftinfo.ArtistName = artist_name
-		nftinfo.ArtistProfilePath = artist_profile_path
-		nftinfo.ArtistAddress = artist_address
-
-		nftinfos.NFTInfos = append(nftinfos.NFTInfos, *nftinfo)
 	}
 
-	jData, err := json.Marshal(nftinfos)
+	upld.Count = len(res)
 
-	if err != nil {
-		fmt.Printf("%s", err)
+	if upld.Count > 0 {
+		tmpl.ExecuteTemplate(w, "uploadfile.html", res)
+	} else {
+		tmpl.ExecuteTemplate(w, "uploadfile.html", nil)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(jData)
 	db.Close()
 }
 
-func handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome to NFTime!\n")
+func uploadFiles(w http.ResponseWriter, r *http.Request) {
+
+	db := dbConn()
+
+	var id int
+
+	r.ParseMultipartForm(200000)
+	if r == nil {
+		fmt.Fprintf(w, "No files can be selected\n")
+	}
+
+	formdata := r.MultipartForm
+
+	fil := formdata.File["files"]
+
+	selDB, err := db.Query("SELECT id FROM file ORDER BY id DESC limit 1")
+	if err != nil {
+		panic(err.Error())
+	}
+	for selDB.Next() {
+		err = selDB.Scan(&id)
+		if err != nil {
+			id = 0
+		}
+	}
+	id = id + 1
+	for i := range fil {
+
+		file, err := fil[i].Open()
+		if err != nil {
+			fmt.Fprintln(w, err)
+			return
+		}
+		defer file.Close()
+
+		fname := fil[i].Filename
+		fsize := fil[i].Size
+		kilobytes := fsize / 1024
+		// megabytes := (float64)(kilobytes / 1024) // cast to type float64
+
+		ftype := fil[i].Header.Get("Content-type")
+		var tempFile *os.File
+
+		text := strings.IndexByte(fname, '.')
+		extension := fname[text:]
+
+		switch extension {
+		case ".png":
+			tempFile, err = ioutil.TempFile("assets/uploadimage", "upload-*.png")
+		case ".jpg":
+			tempFile, err = ioutil.TempFile("assets/uploadimage", "upload-*.jpg")
+		case ".jpeg":
+			tempFile, err = ioutil.TempFile("assets/uploadimage", "upload-*.jpeg")
+		case ".mp4":
+			tempFile, err = ioutil.TempFile("assets/uploadvideo", "upload-*.mp4")
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer tempFile.Close()
+		filepath := tempFile.Name()
+
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		tempFile.Write(fileBytes)
+
+		insForm, err := db.Prepare("INSERT INTO file(id,filename, filesize, filetype, path) VALUES(?,?,?,?,?)")
+		if err != nil {
+			panic(err.Error())
+		} else {
+			log.Println("data insert successfully . . .")
+		}
+		insForm.Exec(id, fname, kilobytes, ftype, filepath)
+
+		log.Printf("Successfully Uploaded File\n")
+		defer db.Close()
+
+		http.Redirect(w, r, "/", 301)
+	}
 }
 
-func swaggerHandler(w http.ResponseWriter, r *http.Request) {
-	swaggerFileUrl := "http://localhost:80/docs/swagger.json"
-	handler := httpSwagger.Handler(httpSwagger.URL(swaggerFileUrl))
-	handler.ServeHTTP(w, r)
+func delete(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	emp := r.URL.Query().Get("id")
+
+	// selDB, err := db.Query("SELECT path from upload where id=?", emp)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// fmt.Println(selDB)
+	// err3 := os.Remove(selDB)
+	fmt.Println(emp)
+	delForm, err := db.Prepare("DELETE FROM file WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	delForm.Exec(emp)
+	log.Println("deleted successfully")
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
 }
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a sample server Petstore server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host petstore.swagger.io
-// @BasePath /v2
-
-func showImg(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	// imgName := req.URL.Query().Get("name")
-	// vars := mux.Vars(req)
-
-	imgName := ps.ByName("imgName")
-	fmt.Println(imgName)
-	text := strings.IndexByte(imgName, '.')
-	extension := imgName[text:]
-	fmt.Println("extension: ", extension)
+func showImg(res http.ResponseWriter, req *http.Request) {
+	imgName := req.URL.Query().Get("name")
 	var path string
-	db := db.DbConn()
+	db := dbConn()
 	selDB, err := db.Query("SELECT path FROM file where filename=?", imgName)
 	if err != nil {
 		panic(err.Error())
@@ -368,20 +200,7 @@ func showImg(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
 			panic(err)
 		}
 	}
-	var filetype string
-	switch extension {
-	case ".png":
-		filetype = "image/png"
-	case ".jpg":
-		filetype = "image/jpg"
 
-	case ".jpeg":
-		filetype = "image/jpeg"
-	case ".gif":
-		filetype = "image/gif"
-	case ".mp4":
-		filetype = "video/mp4"
-	}
 	if imgName != "" && path != "" {
 		path = `./` + path
 
@@ -392,42 +211,28 @@ func showImg(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
 			log.Fatal(err)
 		}
 
-		res.Header().Set("Content-Type", filetype)
+		res.Header().Set("Content-Type", "image/png")
 		res.Write(buf)
 	} else {
 		panic(err)
 	}
+
 }
 
 func main() {
-	// r := chi.NewRouter()
-	router := httprouter.New()
-
-	// router.ServeFiles("/assets/*filepath", http.Dir("assets"))
-	router.ServeFiles("/docs/*filepath", http.Dir("docs"))
+	log.Println("Server started on: http://localhost:80")
+	// fmt.Println(os.Hostname())
+	http.HandleFunc("/dele", delete)
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("assets"))))
-	// router.HandlerFunc(http.MethodGet, "/docs/index.html", swaggerHandler)
-	// router.GET("/docs", swaggerHandler)
-	// docs, err := chai.OpenAPI2(r)
-	// if err != nil {
-	// 	panic(fmt.Sprintf("failed to generate the swagger spec: %+v", err))
-	// }
-	// r.Get("/swagger/*", httpSwagger.Handler(
-	// 	httpSwagger.URL("http://localhost:80/docs/swagger.json"), //The url pointing to API definition
-	// ))
-	// addCustomDocs(docs)
-	// router.GET("/swagger", httpSwagger.WrapHandler)
-	// router.Handler("/swagger", httpSwagger.WrapHandler)
-	// // router.Handle("/swagger", httpSwagger.WrapHandler)
-	// router.HandlerFunc("/swagger", httpSwagger.WrapHandler)
-
-	//http.Handle("/swagger", httpSwagger.WrapHandler)
-	router.GET("/assets/uploadimage/:imgName", showImg)
-	// router.GET("/assets/uploadvideo/:videoName", showVideo)
-	router.GET("/", handler)
-	router.POST("/test", saveHandler)
-	http.HandleFunc("/nft/specific", getNFTInfo)
-	router.GET("/work/full", workInfoSearch)
-	router.GET("/work/specific", specificWork)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	http.HandleFunc("/", upload)
+	http.HandleFunc("/uploadfiles", uploadFiles)
+	http.Handle("/test", http.FileServer(http.Dir("/assets")))
+	http.HandleFunc("/showimg", showImg)
+	// http.Handle("/", http.FileServer(http.Dir("assets/uploadimage")))
+	http.ListenAndServe(":80", nil)
 }
+
+// func index(w http.ResponseWriter, r *http.Request) {
+// 	// io.WriteString(w, "Hello fcc")
+// 	tmplIndex.ExecuteTemplate(w, "index.html", nil)
+// }
