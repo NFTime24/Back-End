5a24878d5b2dcda10b00dbc78233316136e95177
deukyunlee <dukedev1004@gmail.com>
2022-08-03 15:00:23 +0900

    merge: klip action

--- api/klip_action.go
+++ api/klip_action.go
@@ -12,14 +12,14 @@ import (
 
 type KlipResponse struct {
 	RequestKey     string `json:"request_key"`
-	RequestQR      string `json:"request_qr"`
 	Status         string `json:"status"`
 	ExpirationTime int    `json:"expiration_time"`
+	RequestURL     string `json:"request_url"`
 }
 
 func PrepareAuth(c echo.Context) error {
 
-	reqBody := bytes.NewBufferString(`{"bapp":{"name" : "My BApp"}, "callback": { "success": "", "fail": "" }, "type": "auth"}`)
+	reqBody := bytes.NewBufferString(`{"bapp": { "name" : "My BApp" }, "type": "execute_contract", "transaction": { "to": "0xFf1C1e55826DD95C44681BfCd88DCB32eE86B793", "value": "0", "abi": "{\"inputs\": [{\"internalType\": \"string\",\"name\": \"artist_address\",\"type\": \"string\"}],\"name\": \"mintArt\",\"outputs\": [{\"internalType\": \"uint256\",\"name\": \"\",\"type\": \"uint256\"}],\"stateMutability\": \"nonpayable\",\"type\": \"function\"}", "params": "[\"0xF39E4961C046BA913f835c08Bf25De348184F3a8\"]"} }`)
 	resp, err := http.Post("https://a2a-api.klipwallet.com/v2/a2a/prepare", "Content-Type: application/json", reqBody)
 	if err != nil {
 		fmt.Printf(err.Error())
@@ -30,18 +30,20 @@ func PrepareAuth(c echo.Context) error {
 		fmt.Printf(err.Error())
 	}
 	var jData KlipResponse
+	fmt.Printf("body: %s \n", body)
 	json.Unmarshal(body, &jData)
 	// t, err := template.ParseGlob("./templates/qr.html")
 	// if err != nil {
 	// 	fmt.Printf(err.Error())
 	// }
-	jData.RequestQR = "intent://klipwallet/open?url=https://klipwallet.com/?target=/a2a?request_key="
-	jData.RequestQR += "0b0ee0ad-62b3-4146-980b-531b3201265d"
-	jData.RequestQR += "#Intent;scheme=kakaotalk;package=com.kakao.talk;end"
-	fmt.Printf(jData.RequestQR)
+	fmt.Printf("requestkey: %s \n", jData.RequestKey)
+	jData.RequestURL = "intent://klipwallet/open?url=https://klipwallet.com/?target=/a2a?request_key="
+	jData.RequestURL += jData.RequestKey
+	jData.RequestURL += "#Intent;scheme=kakaotalk;package=com.kakao.talk;end"
+	fmt.Printf(jData.RequestURL)
 
 	// http.Redirect(w, r, jData.RequestQR, http.StatusFound)
-	c.Redirect(http.StatusFound, jData.RequestQR)
+	c.Redirect(http.StatusFound, jData.RequestURL)
 	return nil
 	//t.Execute(w, jData)
 }
