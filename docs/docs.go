e0181d4f857c8742e5522a0f1e9e9e725d725cec
deukyunlee <dukedev1004@gmail.com>
2022-08-19 14:48:54 +0900

    test

--- docs/docs.go
+++ docs/docs.go
@@ -178,7 +178,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost",
+	Host:             "34.212.84.161",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "NFTime Sample Swagger API",
