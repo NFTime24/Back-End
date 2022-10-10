d93a5dbb61f2ca62a217db91d83118f01e9fb599
deukyunlee <dukedev1004@gmail.com>
2022-10-10 22:25:10 +0900

    FEAT: gorm fantalk

--- api/artist.go
+++ api/artist.go
@@ -22,9 +22,10 @@ func PostArtist(c echo.Context) error {
 	params := make(map[string]string)
 	test := c.Bind(&params)
 	fmt.Println(test)
+	fmt.Println(params["artist_name"], params["artist_address"], params["artist_profile_id"])
 
-	var artist_id uint
-	var artist_dbId model.Artist
+	var id uint
+	var artist_id model.Artist
 	artist_name := params["artist_name"]
 	artist_address := params["artist_address"]
 	artist_profile_str := params["artist_profile_id"]
@@ -32,14 +33,14 @@ func PostArtist(c echo.Context) error {
 	profile, _ := strconv.ParseUint(artist_profile_str, 10, 32)
 	artist_profile_id := uint(profile)
 
-	db.Model(&artist_dbId).Pluck("ID", &artist_id)
-	artist_id += 1
-	fmt.Println(artist_id)
+	db.Model(&artist_id).Pluck("ID", &id)
+	id += 1
+	fmt.Println(id)
 
-	artist_insert := model.Artist{ID: artist_id, Name: artist_name, Address: artist_address, ProfileID: artist_profile_id}
+	artist_insert := model.Artist{ID: id, Name: artist_name, Address: artist_address, ProfileID: artist_profile_id}
 
 	db.Create(&artist_insert)
-	return c.JSON(http.StatusOK, params)
+	return c.JSON(http.StatusOK, params["artist_name"])
 }
 
 // @Summary artist info
@@ -67,7 +68,62 @@ func ShowAllArtists(c echo.Context) error {
 	}
 	for rows.Next() {
 		db.ScanRows(rows, &results)
+		fmt.Println(results[1])
 	}
 
+	return c.JSON(http.StatusOK, results)
+}
+
+func GetActiveArtists(c echo.Context) error {
+	type Result struct {
+		Id        uint   `json:"id"`
+		Name      string `json:"name"`
+		Address   string `json:"address"`
+		ProfileId uint   `json:"profile_id"`
+		Path      string `json:"path"`
+	}
+
+	db := db.DbManager()
+	var results []Result
+	rows, err := db.Select(`a.*`).
+		Table(`artists as a`).
+		Joins("join works as w on w.artist_id = a.id").
+		Group("a.id").Rows()
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		db.ScanRows(rows, &results)
+	}
+	return c.JSON(http.StatusOK, results)
+}
+
+func GetTopArtists(c echo.Context) error {
+	type Result struct {
+		Id        uint   `json:"id"`
+		Name      string `json:"name"`
+		Address   string `json:"address"`
+		ProfileId uint   `json:"profile_id"`
+		Path      string `json:"path"`
+	}
+
+	db := db.DbManager()
+	var results []Result
+	rows, err := db.Select(`a.*, f.path`).
+		Table(`(
+			select w.artist_id, count(w.artist_id) as count
+			from test.nfts as n
+			left join test.works as w on w.work_id = n.works_id
+			group by w.artist_id
+			order by count desc
+			limit 5 ) as base`).
+		Joins("left join artists as a on base.artist_id = a.id").
+		Joins("left join files as f on f.id = a.profile_id").Rows()
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		db.ScanRows(rows, &results)
+	}
 	return c.JSON(http.StatusOK, results)
 }
